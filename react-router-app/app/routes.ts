7621a34c09a4a87c96e36d79220346221844143e
Marty Bonacci <martybonacci@gmail.com>
2025-05-06 12:11:22 -0600

    merged home and welcome

--- react-router-app/app/routes.ts
+++ react-router-app/app/routes.ts
@@ -1,11 +1,8 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {type RouteConfig, index, route} from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-    // route to about page
+    index("routes/home.tsx"),
     route("/about", "routes/about.tsx"),
-    // route to contact page
     route("/contact", "routes/contact.tsx"),
-    // rout to services page
     route("/services", "routes/services.tsx"),
 ] satisfies RouteConfig;
